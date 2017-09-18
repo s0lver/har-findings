e60bf8eb02e6c4d82de5abd665a011b2789f678c
s0lver <rperez.github@gmail.com>
2017-09-18 18:57:35 -0500

    Initial commit

---
+++ acc_data_reader.py
@@ -0,0 +1,9 @@
+# The structure is:
+# id_window, x, y, z, timestamp
+# For instance:
+# 1,-7.4529877,-6.203964,-3.3417053,1478123867539
+
+# 1. Read data
+# 2. Then plot each axis
+# 3. Then plot the magnitude vector
+# 4. Try to observe something
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Accelerometer findings
+
+This is a project for finding out information from the sampled accelerometer data.
+Plots are generated.
